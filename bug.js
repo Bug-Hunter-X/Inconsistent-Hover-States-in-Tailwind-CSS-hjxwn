f44724685cec823cd54f5f2836257ce859f4d951
BugHunter-X <askzenai@gmail.com>
2025-01-23 23:20:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+```javascript
+// Example showing incorrect Tailwind directives usage
+<div class="bg-blue-500 p-4 text-white">
+  <p class="text-xl font-bold">This is a paragraph</p>
+  <button class="bg-gray-800 hover:bg-gray-900 text-white font-bold py-2 px-4 rounded">
+    Click me!
+  </button>
+</div>
+```
